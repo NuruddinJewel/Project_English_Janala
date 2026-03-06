ea869828cba65dc48afbd06145c1b80798454870
Jewel <m.76jewel@gmail.com>
2026-03-06 06:09:08 +0600

    Lessons Section

--- script/index.js
+++ script/index.js
@@ -1,8 +1,14 @@
 const loadLessons=()=>{
     fetch("https://openapi.programming-hero.com/api/levels/all") //promise of response
     .then(res=>res.json()) //promise of json
-    .then((json)=> displayLesson(json.data))
+    .then((json)=> displayLesson(json.data));
 };
+
+
+
+
+
+
 // Stage-2(Calling)
 const loadLevelWord=(id)=>{
     // console.log(id);
@@ -10,23 +16,53 @@ const loadLevelWord=(id)=>{
     // console.log(url);
     fetch(url)
     .then((res)=>res.json())
-    .then((data)=>displayLevelWord(data.data));
+    .then((data)=>{
+        displayLevelWord(data.data)});
 
 };
+
 //Stage-2 (Receive words)
 const displayLevelWord=(words)=>{
     // console.log(words);
     const wordContainer = document.getElementById("word-container");
-    // wordContainer.innerHtml="";//innerHtml empty
+    wordContainer.innerHTML=" ";//innerHTML empty
+    if(words.length==0){
+        // alert("no word detected");
+        wordContainer.innerHTML=`<div class="text-center col-span-full rounded py-8 space-y-4 font-bangla">
+        <img class="mx-auto"  src="./assets/alert-error.png"/>
+        <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+    <h2 class="font-bold text-3xl">নেক্সট Lesson এ যান</h2>
+</div>`;
+        return;
+    }
+//     {
+//     "id": 5,
+//     "level": 1,
+//     "word": "Eager",
+//     "meaning": "আগ্রহী",
+//     "pronunciation": "ইগার"
+// }
+
+
+
     words.forEach((word) => {
         console.log(word);
     const card=document.createElement("div");
     card.innerHTML=`
-    <p>Apple</p>`;
+    <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-2">
+    <h2 class="font-bold text-2xl">${word.word}</h2>
+    <p class="font-semibold">Meaning / Pronunciation</p>
+    <div class="text-2xl font-medium font-bangla">${word.meaning} / 
+    ${word.pronunciation}</div>
+    <div class="flex justify-between items-center">
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+    </div>
+</div>`;
     wordContainer.append(card);
     })
 
-}
+};
 
 // Display
 const displayLesson=(lessons)=>{
@@ -39,9 +75,9 @@ for(let lesson of lessons) {
 console.log(lesson)
         // a) Create Element
 const btnDiv = document.createElement("div");
-btnDiv.innerHTML=`<button onclick="loadLevelWord
-(${lesson.level_no})" href="" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
-</button></li>`
+btnDiv.innerHTML= `<button onclick="loadLevelWord
+(${lesson.level_no})"  class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+</button>`
 
         // b) Append into container
 
